4bec53840ab12ceb2dc1f887101c2f689d1a82a0
imakiri <vanzha.alexander@gmail.com>
2022-08-17 21:08:57 +0300

    Fixed name collision

--- bitsdojo_window_windows/lib/src/win32_plus.dart
+++ bitsdojo_window_windows/lib/src/win32_plus.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'dart:ui';
-import 'package:win32/win32.dart';
+import 'package:win32/win32.dart' hide Rect, Size;
 
 extension RECTtoRect on RECT {
-  Rect get toRect => Rect.fromLTRB(this.left.toDouble(), this.top.toDouble(),
-      this.right.toDouble(), this.bottom.toDouble());
+  Rect get toRect => Rect.fromLTRB(this.left.toDouble(), this.top.toDouble(), this.right.toDouble(), this.bottom.toDouble());
 }
 
 extension SIZEtoSize on SIZE {
